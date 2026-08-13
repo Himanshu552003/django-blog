897d5b12a0fe152513d4e84aed8c253b9aa964f4
Himanshu552003 <yadavhimanshu552003@gmail.com>
2026-08-13 16:28:13 +0530

    posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from blogs.models import Category, Blog
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    # to fetch data from the db
+    categories=Category.objects.all()
+    featured_posts=Blog.objects.filter(is_featured=True, status="Published").order_by('updated_at')
+    posts=Blog.objects.filter(is_featured=False, status="Published")
+    context={
+        "categories":categories,
+        "featured_posts": featured_posts,
+        "posts":posts,
+
+    
+    }
+    return render(request, 'home.html', context)
